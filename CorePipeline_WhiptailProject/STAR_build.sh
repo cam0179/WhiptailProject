14e31f2e77527200d065c065d22127d9abe4b692
mag0099 <114441712+mag0099@users.noreply.github.com>
2023-04-14 06:33:10 -0500

    Create STAR_build.sh

---
+++ CorePipeline_WhiptailProject/STAR_build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+#SBATCH --job-name=Star_idx
+#SBATCH --output=logs/Star-idx_%A.log
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=100gb
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --qos=large
+#SBATCH --constraint=dmc
+#SBATCH --mail-user=mag0099@auburn.edu
+#SBATCH --mail-type=ALL
+
+
+module load star/2.7.0e
+module load samtools/1.13
+
+workDir=/scratch/WhiptailProject
+
+baseName=${workDir}/Reference/P_raffonei/rPodRaf1
+outIndex=${workDir}/Reference/P_raffonei/Star_idx
+
+mkdir -p ${workDir}/Reference/P_raffonei/Star_idx
+
+STAR --runThreadN 20 \
+--runMode genomeGenerate \
+--genomeDir $outIndex \
+--genomeFastaFiles $baseName.fna \
+--sjdbGTFfile $baseName.gtf \
+--sjdbOverhang -1
